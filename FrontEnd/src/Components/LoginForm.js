a76d1b655063a819e699c0d1126b0a35a40c365f
Peng <penghe201211@gmail.com>
2023-07-06 13:05:31 -0300

    promises working

--- FrontEnd/src/Components/LoginForm.js
+++ FrontEnd/src/Components/LoginForm.js
@@ -1,26 +1,32 @@
 import React, { useState } from 'react';
 import './LoginForm.css';
-import CreateStoriesModal from './CreateStoriesModal';
 import { loginUser } from '../Services/userService';
 
 function LoginForm(props) {
-  const [showCreateStoryModal, setShowCreateStoryModal] = useState(false);
-  const [email, setEmail]=useState('')
-  const [password,setPassword]=useState('')
-  const handleOk=()=>{
-    loginUser(email,password);
-    props.setIsUserIdendified(true);
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loginResult, setLoginResult] = useState('')
+
+
+  function handleOk() {
+    loginUser(email, password)
+    .then(response => {
+      setLoginResult(response);
+      if (loginResult === null) {
+        props.setIsUserIdendified(true);
+      }
+    })
+
   }
 
   return (
     <div className="login-form">
       <h1>Log In</h1>
-      <input type='email'  value={email} onChange={e => setEmail(e.target.value)} placeholder='enter your email' name='email'/><br></br>
-        <input type= 'password'   value={password} onChange={e => setPassword(e.target.value)} placeholder=' enter your password' name='password'/> 
-      <button className='register-btn' onClick={handleOk}>Ok</button>
+      <input type='email' value={email} onChange={e => setEmail(e.target.value)} placeholder='enter your email' name='email' /><br></br>
+      <input type='password' value={password} onChange={e => setPassword(e.target.value)} placeholder=' enter your password' name='password' />
+      <button className='register-btn' onClick={() => handleOk()}>Ok</button>
       <p>Create account?</p> <a style={{ color: "red", cursor: 'pointer' }} onClick={() => props.setIsUserRegistered(false)}>Signup</a>
-     
-
+      <p className="text-danger">adffad{loginResult}</p>
     </div>
 
 
--- FrontEnd/src/Services/userService.js
+++ FrontEnd/src/Services/userService.js
@@ -17,24 +17,24 @@ export function createUser(name, email, password) {
 
 
  
-  export function loginUser(email, password){
-    axios
+  export  function loginUser(email, password){
+     return axios
     .post(`${baseURL}/login`,{
       email:email,
       password:password
     })
-    .then(response =>
-      console.log (response.data)
-    )
+    .then(response =>{
+      return null;
+    })
     .catch(error =>{
       if (error.response.status ==401) {
-        console.log("password incorrect")
+        return "password incorrect";
         
       } else if (error.response.status ==404){
-        console.log("email incorrect")
+        return "email incorrect";
         
       }else{
-        console.log("unknown error")
+       return "unknown error";
       }
     })
    
