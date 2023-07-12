ad648c4d68a6c2542d4c50306eec91e11e00dd9e
Peng <penghe201211@gmail.com>
2023-07-12 15:57:30 -0300

    "all works"

--- BackEnd/Entities/Story.cs
+++ BackEnd/Entities/Story.cs
@@ -1,11 +1,13 @@
-﻿namespace ApiStories.Entities
+﻿using ApiStories.Interfaces;
+
+namespace ApiStories.Entities
 {
-    public class Story
+    public class Story 
     {
         public string Type = nameof(Story);
         public string Id { get; set; }
         public string Title { get; set; }
-        public string Classification { get; set; }
+        public string Category { get; set; }
         public int AgeAppropiate { get; set; }
         public string Description { get; set; }
     }
--- BackEnd/Models/CreateStoryModel.cs
+++ BackEnd/Models/CreateStoryModel.cs
@@ -3,7 +3,7 @@
     public class CreateStoryModel
     {
         public string Title { get; set; }
-        public string Classification { get; set; }
+        public string Category { get; set; }
         public int AgeAppropiate { get; set; }
         public string Description { get; set; }
     }
